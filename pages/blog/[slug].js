0af73241bd2c9dc0c41d27f15522a1106b23f3dc
chidera <greyspades99@gmail.com>
2022-01-19 19:25:56 +0100

    added type checks for the blog page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -40,18 +40,18 @@ const Post = ({post}) => {
   
   <div className='bg-[#9BAFCD] md:px-10 md:p-6 md:col-span-3 rounded-lg'>
   
-  {/* <div className='justify-center grid'>
-      <img className='w-[500px] h-[200px] md:w-[950px] md:h-[400px]' src={getImage(post.mainImage).url()} />
-  </div> */}
+  <div className='justify-center grid'>
+      <img className='w-[500px] h-[200px] md:w-[950px] md:h-[400px]' src={getImage(post?.mainImage).url()} />
+  </div>
 
   <h1 className='md:text-3xl text-2xl font-bold m-4'>
-    {post.title}
+    {post?.title}
   </h1>
 
   <div className='text-xl m-6'>
   <BlockContent
   
-    blocks={post.body}
+    blocks={post?.body}
     imageOptions={{ w: 320, h: 240, fit: 'max' }}
     {...client.config()}
   />
